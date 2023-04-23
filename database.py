caac61b519640ccfbbf9a9dc557fc917e4e9f27c
dizzysnazzy <32930626+dizzysnazzy@users.noreply.github.com>
2023-04-23 19:02:14 +0000

    changes in string formatting

--- database.py
+++ database.py
@@ -25,7 +25,7 @@ def load_job_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(
       text("SELECT * FROM jobs WHERE id=:val"),
-      {"val":id}
+      val=id
     )
     rows = result.all()
     if len(rows) == 0:
