70f123214834b7ad2dc010b53129423d1d8dad42
dizzysnazzy <32930626+dizzysnazzy@users.noreply.github.com>
2023-04-24 15:02:27 +0000

    Store Application in database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -26,6 +26,18 @@ def show_job(id):
     return "Not Found", 404
     
   return render_template('jobpage.html',
+                         job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):  
+  data = request.form
+  job=load_job_from_db(id)
+  add_application_to_db(id, data)
+  # store this in the db
+  # send an email
+  # display an acknowledgement
+  return render_template('application_submitted.html',
+                         application=data,
                          job=job)
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -15,8 +15,8 @@ engine = create_engine(
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
-    jobs = []
     
+    jobs = []    
     for row in result:
       jobs.append(row._asdict())      
     return jobs
@@ -30,3 +30,16 @@ def load_job_from_db(id):
       return None
     else:
       return rows[0]._asdict()
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    conn.execute(query, {
+                 'job_id' : job_id,            
+                 'full_name' : data['full_name'],
+                 'email' : data['email'],
+                 'linkedin_url' : data['linkedin_url'],
+                 'education' : data['education'],
+                 'work_experience' : data['work_experience'],
+                 'resume_url' : data['resume_url']})
