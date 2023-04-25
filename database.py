66df3016dfd1fa59c40ce97cb9897f74a86e7a30
dizzysnazzy <32930626+dizzysnazzy@users.noreply.github.com>
2023-04-25 14:54:27 +0000

    Changed variable 'job' to 'item'

--- database.py
+++ database.py
@@ -14,7 +14,7 @@ engine = create_engine(
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
+    result = conn.execute(text("SELECT * FROM jobs"))
     
     jobs = []    
     for row in result:
@@ -24,8 +24,8 @@ def load_jobs_from_db():
 def load_job_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(
-      text(f"SELECT * FROM jobs WHERE id = {id}"))    
-    rows = result.all()
+      text(f"SELECT * FROM jobs WHERE id = {id}"))
+    rows = result.all()    
     if len(rows) == 0:
       return None
     else:
